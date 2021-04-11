19c0e5b9ae780bf2b951a71538bf4faa3e981701
rupali-talele <rupali.talele1995@gmail.com>
2021-04-10 23:47:34 -0700

    Updated quotes

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ window.onload = function ()
     var positivity = document.getElementsByClassName("positivity")[0]
 
     console.log("calling home")    
-    home()
+    positive()
 }
 
 function heal()
@@ -32,7 +32,7 @@ function heal()
 }
 
 
-function home()
+function positive()
 {
    fetch('./positivity.json')
     .then(res => res.json())
@@ -56,9 +56,9 @@ function home()
 }).catch(err => console.error(err));    
 }
 
-function positivity()
+function success()
 {
-   fetch('./positivity.json')
+   fetch('./success.json')
     .then(res => res.json())
     .then((out) => {
         container.innerHTML = ""
@@ -80,9 +80,9 @@ function positivity()
 }).catch(err => console.error(err));    
 }
 
-function life()
+function strength()
 {
-   fetch('./life.json')
+   fetch('./strength.json')
     .then(res => res.json())
     .then((out) => {
         container.innerHTML = ""
@@ -107,6 +107,54 @@ function life()
 function morning()
 {
    fetch('./morning.json')
+    .then(res => res.json())
+    .then((out) => {
+        container.innerHTML = ""
+        for(idx in out["quotes"])
+        {
+            console.log(out["quotes"][idx])
+            let div = document.createElement('div')
+            div.classList.add("jumbotron")
+            div.classList.add("jumbotron-fluid")
+            let blockquote = document.createElement('blockquote')
+            blockquote.classList.add("blockquote")
+            div.appendChild(blockquote)
+            let p =  document.createElement('p')
+            p.innerHTML = out["quotes"][idx]
+            blockquote.appendChild(p)
+            container.appendChild(div)
+        }
+        console.log('Output: ', out);
+}).catch(err => console.error(err));    
+}
+
+function selfworth()
+{
+   fetch('./selfworth.json')
+    .then(res => res.json())
+    .then((out) => {
+        container.innerHTML = ""
+        for(idx in out["quotes"])
+        {
+            console.log(out["quotes"][idx])
+            let div = document.createElement('div')
+            div.classList.add("jumbotron")
+            div.classList.add("jumbotron-fluid")
+            let blockquote = document.createElement('blockquote')
+            blockquote.classList.add("blockquote")
+            div.appendChild(blockquote)
+            let p =  document.createElement('p')
+            p.innerHTML = out["quotes"][idx]
+            blockquote.appendChild(p)
+            container.appendChild(div)
+        }
+        console.log('Output: ', out);
+}).catch(err => console.error(err));    
+}
+
+function motivation()
+{
+   fetch('./motivation.json')
     .then(res => res.json())
     .then((out) => {
         container.innerHTML = ""
