e95f6d5fc09aade2fdc15ddf1b9a33f7429fe584
rupali-talele <rupali.talele1995@gmail.com>
2021-04-10 17:37:15 -0700

    Quotes

--- app.js
+++ app.js
@@ -3,12 +3,110 @@ window.onload = function ()
     var container = document.getElementById("container")
     var positivity = document.getElementsByClassName("positivity")[0]
 
+    console.log("calling home")    
+    home()
+}
+
+function heal()
+{
+   fetch('./healing.json')
+    .then(res => res.json())
+    .then((out) => {
+        container.innerHTML = ""
+        for(idx in out["quotes"])
+        {
+            console.log(out["quotes"][idx])
+            let div = document.createElement('div')
+            div.classList.add("jumbotron")
+            div.classList.add("jumbotron-fluid")
+            let blockquote = document.createElement('blockquote')
+            blockquote.classList.add("blockquote")
+            div.appendChild(blockquote)
+            let p =  document.createElement('p')
+            p.innerHTML = out["quotes"][idx]
+            blockquote.appendChild(p)
+            container.appendChild(div)
+        }
+        console.log('Output: ', out);
+}).catch(err => console.error(err));    
+}
+
+
+function home()
+{
+   fetch('./positivity.json')
+    .then(res => res.json())
+    .then((out) => {
+        container.innerHTML = ""
+        for(idx in out["quotes"])
+        {
+            console.log(out["quotes"][idx])
+            let div = document.createElement('div')
+            div.classList.add("jumbotron")
+            div.classList.add("jumbotron-fluid")
+            let blockquote = document.createElement('blockquote')
+            blockquote.classList.add("blockquote")
+            div.appendChild(blockquote)
+            let p =  document.createElement('p')
+            p.innerHTML = out["quotes"][idx]
+            blockquote.appendChild(p)
+            container.appendChild(div)
+        }
+        console.log('Output: ', out);
+}).catch(err => console.error(err));    
 }
 
 function positivity()
 {
-//    fetch('https://github.com/rupali-talele/Quotes/blob/main/breakup.json')
-   fetch('./breakup.json')
+   fetch('./positivity.json')
+    .then(res => res.json())
+    .then((out) => {
+        container.innerHTML = ""
+        for(idx in out["quotes"])
+        {
+            console.log(out["quotes"][idx])
+            let div = document.createElement('div')
+            div.classList.add("jumbotron")
+            div.classList.add("jumbotron-fluid")
+            let blockquote = document.createElement('blockquote')
+            blockquote.classList.add("blockquote")
+            div.appendChild(blockquote)
+            let p =  document.createElement('p')
+            p.innerHTML = out["quotes"][idx]
+            blockquote.appendChild(p)
+            container.appendChild(div)
+        }
+        console.log('Output: ', out);
+}).catch(err => console.error(err));    
+}
+
+function life()
+{
+   fetch('./life.json')
+    .then(res => res.json())
+    .then((out) => {
+        container.innerHTML = ""
+        for(idx in out["quotes"])
+        {
+            console.log(out["quotes"][idx])
+            let div = document.createElement('div')
+            div.classList.add("jumbotron")
+            div.classList.add("jumbotron-fluid")
+            let blockquote = document.createElement('blockquote')
+            blockquote.classList.add("blockquote")
+            div.appendChild(blockquote)
+            let p =  document.createElement('p')
+            p.innerHTML = out["quotes"][idx]
+            blockquote.appendChild(p)
+            container.appendChild(div)
+        }
+        console.log('Output: ', out);
+}).catch(err => console.error(err));    
+}
+
+function morning()
+{
+   fetch('./morning.json')
     .then(res => res.json())
     .then((out) => {
         container.innerHTML = ""
